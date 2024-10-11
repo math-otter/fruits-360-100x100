6924f81cbf0bfbb06a7f3ffab4a41ad601fd1b25
math-otter <yeonung0315@gmail.com>
2024-10-11 22:41:28 +0900

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,22 @@
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 import os
 
 image_folder = "Training\Banana 1"
+
 all_files = os.listdir(image_folder)
-print(all_files[0])
+
+image_data = []
+
+for file_name in all_files:
+    
+    if file_name.endswith("_100.jpg"):
+        image_path = os.path.join(image_folder, file_name)
+        img = Image.open(image_path).convert("L")
+        img_array = np.array(img)
+        image_data.append(img_array)
+
+image_data = np.stack(image_data, axis=0)
 
 
 # image_files = [f"r0_{i}_100.jpg" for i in range(0, 100)]
