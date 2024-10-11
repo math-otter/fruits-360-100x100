e2109d76cf480b37018d06f1ee8901d8f8abac0e
math-otter <yeonung0315@gmail.com>
2024-10-11 23:08:26 +0900

    이미지 로드 모듈 테스트

--- module test.py
+++ module test.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as plt
 from imgtoarray import load_images
 bananas = load_images("Training\Banana 1")
 apples = load_images("Training\Apple Red 1")
 print(bananas.shape)
 print(apples.shape)
+
+plt.imshow(apples[0], cmap="gray")
+plt.show()
