56639de414524fdce17a46ced253f201aa0ea566
math-otter <yeonung0315@gmail.com>
2024-10-13 20:54:06 +0900

    Update module test.py

--- module test.py
+++ module test.py
@@ -4,7 +4,7 @@ from imgtoarray import load_images
 bananas = load_images("Training\Banana 1")
 apples = load_images("Training\Apple Red 1")
 
-print(bananas.shape, apples.shape)
+print(bananas.shape)
 
 plt.imshow(apples[0], cmap="gray_r")
 plt.show()
